3e45972b4a94286dce0f94e41423d536f2ec8d0c
michaelborisov <michaelborisovha@gmail.com>
2018-05-06 15:19:33 +0200

    Bug-fixing + comments added

--- project_1/task1.3/connect_four_game.py
+++ project_1/task1.3/connect_four_game.py
@@ -6,6 +6,7 @@ class ConnectFourGame:
     def __init__(self):
         self.game_state = np.zeros((n_rows, n_columns), dtype=int)
 
+    """Prints current game state"""
     def print_game_state(self):
         state_matrix = np.copy(self.game_state.astype(object))
 
@@ -14,48 +15,55 @@ class ConnectFourGame:
         print state_matrix
         print "\n"
 
+    """
+    If parameter column is equal to None, that means, that
+    move is made at random.
+    """
     def make_move(self, p, column=None):
         if column is None:
             column = self.__select_column_at_random()
 
+        # Slicing the whole target column and finding first available
+        # row for insertion of a new element.
         chosen_column = self.game_state[:, column]
         desired_row = -1
         for i, e in list(enumerate(chosen_column)):
             if e == 0:
                 desired_row = i
 
+        # If an empty cell was found
         if desired_row != -1:
             self.game_state[desired_row, column] = p
 
-        if self.__move_was_winning_move(p):
-            print "last player wins"
+    """This method checks whether one player has won the game"""
+    def move_was_winning_move(self):
 
-    def __move_was_winning_move(self, p):
-
-        # y_axis_sums = (np.sum(self.game_state, axis=1)) * p
-        # for i, y_sum in list(enumerate(y_axis_sums)):
+        # Check all rows if there is a winning combination
         for i in range(n_rows):
-            # if y_sum == winning_sum:
             row_to_check = self.game_state[i, :]
             if (row_to_check == 0).sum() <= 3:
                 if self.__check_all_sublists(row_to_check):
                     return True
 
-        # x_axis_sums = (np.sum(self.game_state, axis=0)) * p
-        # for i, x_sum in list(enumerate(x_axis_sums)):
+        # Check all columns if there is a winning combination
         for i in range(n_columns):
-            # if x_sum == winning_sum:
             column_to_check = self.game_state[:, i]
             if (column_to_check == 0).sum() <= 3:
                 if self.__check_all_sublists(column_to_check):
                     return True
 
+        # TODO proper naming in comments required
+
+        # Check all diagonals of length at least 4
+        # if there is a winning combination
         for i in diagonal_shifts:
             diagonal = self.game_state.diagonal(i)
             if (diagonal == 0).sum() <= 3:
                 if self.__check_all_sublists(diagonal):
                     return True
 
+        # Check all non-main diagonals of length at least 4
+        # if there is a winning combination
         for i in diagonal_shifts:
             diagonal = np.rot90(self.game_state).diagonal(i)
             if (diagonal == 0).sum() <= 3:
@@ -63,6 +71,8 @@ class ConnectFourGame:
                     return True
         return False
 
+    """Constructs sublists of given list and
+    check whether there is a winning combination for sublists with length 4"""
     def __check_all_sublists(self, list_to_check):
         sublists = [list_to_check[i:i + winning_sum] for i in
                     range(0, len(list_to_check), 1)]
@@ -70,15 +80,17 @@ class ConnectFourGame:
         for sublist in sublists:
             if len(sublist) < winning_sum:
                 continue
-            if np.sum(sublist) == winning_sum:
+            if np.abs(np.sum(sublist)) == winning_sum:
                 return True
 
         return False
 
-    def __select_column_at_random(self):
-        xs, ys = np.where(self.game_state == 0)
-        i = np.random.permutation(np.arange(xs.size))[0]
-        return xs[i]
-
+    """Checks whether there are still empty cells in matrix"""
     def move_still_possible(self):
         return not (self.game_state[self.game_state == 0].size == 0)
+
+    """Returns index of randomly chosen column for element insertion"""
+    def __select_column_at_random(self):
+        xs, ys = np.where(self.game_state == 0)
+        i = np.random.permutation(np.arange(ys.size))[0]
+        return ys[i]
--- project_1/task1.3/task1.3.py
+++ project_1/task1.3/task1.3.py
@@ -1,9 +1,9 @@
 import numpy as np
 from connect_four_game import ConnectFourGame
+from constants import *
 
 
-if __name__ == '__main__':
-
+def test_run():
     game = ConnectFourGame()
     game.print_game_state()
 
@@ -26,3 +26,41 @@ if __name__ == '__main__':
     # game.make_move(1, 6)
     game.print_game_state()
 
+
+def run_random_game():
+    game = ConnectFourGame()
+
+    # initialize player number, move counter
+    player = 1
+    mvcntr = 1
+
+    # initialize flag that indicates win
+    no_winner_yet = True
+
+    while game.move_still_possible() and no_winner_yet:
+        # get player symbol
+        name = symbols[player]
+        print '%s moves' % name
+
+        # let player move at random
+        game.make_move(player)
+
+        # print current game state
+        game.print_game_state()
+
+        # evaluate game state
+        if game.move_was_winning_move():
+            print 'player %s wins after %d moves' % (name, mvcntr)
+            no_winner_yet = False
+
+        # switch player and increase move counter
+        player *= -1
+        mvcntr += 1
+
+    if no_winner_yet:
+        print 'game ended in a draw'
+
+
+if __name__ == '__main__':
+    print __name__
+    run_random_game()
